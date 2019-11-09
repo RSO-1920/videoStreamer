c3c130c5108fdbd1bbc51ef172b5c45dc16c7e38
Uroš Zoretič <uz1734@student.uni-lj.si>
2019-11-09 21:08:22 +0100

    video-streamer service discovery and k8 deployment

--- video-streamer-api/src/main/java/si/fri/rso/api/v1/VideoStreamingAPI.java
+++ video-streamer-api/src/main/java/si/fri/rso/api/v1/VideoStreamingAPI.java
@@ -1,8 +1,10 @@
 package si.fri.rso.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService(value = "rso1920-catalog")
 @ApplicationPath("/v1")
 public class VideoStreamingAPI extends Application {
 }
