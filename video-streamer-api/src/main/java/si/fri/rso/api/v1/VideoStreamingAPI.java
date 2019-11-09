1cec9d4ae6468ea6f7ce992e3af15d290b1dd1ba
Uroš Zoretič <uz1734@student.uni-lj.si>
2019-11-09 21:12:02 +0100

    video streamer service discovery

--- video-streamer-api/src/main/java/si/fri/rso/api/v1/VideoStreamingAPI.java
+++ video-streamer-api/src/main/java/si/fri/rso/api/v1/VideoStreamingAPI.java
@@ -4,7 +4,7 @@ import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@RegisterService(value = "rso1920-catalog")
+@RegisterService(value = "rso1920-video-streamer")
 @ApplicationPath("/v1")
 public class VideoStreamingAPI extends Application {
 }
