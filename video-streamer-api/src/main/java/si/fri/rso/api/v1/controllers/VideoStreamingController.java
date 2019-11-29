1a7387f6c42a4d49b8d36319987f80223583320d
Uroš Zoretič <uz1734@student.uni-lj.si>
2019-11-29 12:11:48 +0100

    logginng

--- video-streamer-api/src/main/java/si/fri/rso/api/v1/controllers/VideoStreamingController.java
+++ video-streamer-api/src/main/java/si/fri/rso/api/v1/controllers/VideoStreamingController.java
@@ -1,5 +1,8 @@
 package si.fri.rso.api.v1.controllers;
 
+import com.kumuluz.ee.logs.cdi.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.microprofile.metrics.Histogram;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Metered;
@@ -12,20 +15,27 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
 
+@Log
 @ApplicationScoped
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class VideoStreamingController {
     private static final String FILE_PATH = "./file_example_MP4_1920_18MG.mp4";
+    private static final Logger LOG = LogManager.getLogger(VideoStreamingController.class.getName());
 
     @Inject
     StreamBean streamBean;
+
+    @Context
+    ContainerRequestContext reqContext;
 
     @Inject
     @Metric(name = "histogram_streaming")
@@ -37,6 +47,9 @@ public class VideoStreamingController {
     @Produces("video/mp4")
     public Response header() {
         File file = new File(FILE_PATH);
+        LOG.info("Inside head method");
+
+        System.out.println("req context property: " + reqContext.getProperty("uniqueRequestId"));
 
         return Response.ok()
                 .status(Response.Status.PARTIAL_CONTENT)
